aa4dd80947bb14e2fa21867b5e0fb6baec4af5a0
Yaregal-Tadeese-Tessema <yaregal@garrilogistics.com>
2025-05-05 21:30:14 +0300

    test

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,6 @@ export const loginUser = async (username: string, password: string) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
   });
-
   if (!res.ok) {
     throw new Error("Invalid username or password");
   }
